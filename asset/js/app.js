9f45de7ecd03f2fe4d0801bf4d3b9a02fe7b108d
phduong65 <phamduong652003@gmail.com>
2023-07-17 13:39:01 +0700

    first commit

--- asset/js/app.js
+++ asset/js/app.js
@@ -14,7 +14,16 @@ $('.group_card_people').slick({
           arrows: false,
         }
         
-      },
+      },{
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+        }
+        
+      }
       
     ]
   });
@@ -99,12 +108,86 @@ $(document).ready(function () {
 $(document).ready(function(){
   $('.discover .group_control .btn_filter').click(function(e){
     if ( $('.discover .group_control .btn_filter .bi-x').length) {
-      $('.discover .group_control .btn_filter .bi-x').remove();  
+      $('.discover .group_control .btn_filter .bi-x').remove();
+      if ($('.discover .discover_filter .discover_sort').length) {
+        $('.discover .discover_filter .discover_sort').remove();
+      } 
     }
     else{
       let text = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
       <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
     </svg>`;
+    let filter = `<div class="discover_sort">
+    <div class="discover_cell discover_price">
+        <div class="discover_label">Price</div>
+        <div class="dropdown">
+            <button class="btn btn_dropdown" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Sellers
+                <div class="arrow-down">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                </div>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-lg-start dropdown-menu-end">
+                <li><span class="dropdown-item" href="#">Sellers</span></li>
+                <li><span class="dropdown-item" href="#">Buyers</span></li>
+            </ul>
+        </div>
+    </div>
+    <div class="discover_cell dicover_likes">
+        <div class="discover_label">Likes</div>
+        <div class="dropdown">
+            <button class="btn btn_dropdown" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Sellers
+                <div class="arrow-down">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                </div>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-lg-start dropdown-menu-end">
+                <li><span class="dropdown-item" href="#">Sellers</span></li>
+                <li><span class="dropdown-item" href="#">Buyers</span></li>
+            </ul>
+        </div>
+    </div>
+    <div class="discover_cell discover_creator">
+        <div class="discover_label">Creator</div>
+        <div class="dropdown">
+            <button class="btn btn_dropdown" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Sellers
+                <div class="arrow-down">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                </div>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-lg-start dropdown-menu-end">
+                <li><span class="dropdown-item" href="#">Sellers</span></li>
+                <li><span class="dropdown-item" href="#">Buyers</span></li>
+            </ul>
+        </div>
+    </div>
+    <div class="discover_cell discover_price-rg">
+        <label for="customRange2" class="form-label discover_label">Price range</label>
+        <input type="range" class="form-range" min="0.01" max="10.0" id="customRange2">
+        <div class="discover_scale">
+            <div>0.01 ETH</div>
+            <div>10 ETH</div>
+        </div>
+    </div>
+</div>`
+      $('.discover .discover_filter').append(filter);
       $('.discover .group_control .btn_filter .filter_ic').append(text);
     }
   });
